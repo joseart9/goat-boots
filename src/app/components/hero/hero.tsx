7c8df4fdab217d15b11be8f452e02357f271f6a5
Arturo <a01177145@tec.mx>
2025-02-18 18:38:22 -0600

    Changes

--- src/app/components/hero/hero.tsx
+++ src/app/components/hero/hero.tsx
@@ -1,7 +1,19 @@
+import { Button } from '@/components/shared/ui/button';
+import { LandingPrimaryImageCtaSection } from '@/components/landing/cta/LandingPrimaryCta';
+
 export default function Hero() {
     return (
         <section className="hero">
-            <h1>Hero Section</h1>
+            <LandingPrimaryImageCtaSection
+                title="Calzado industrial de alta calidad"
+                description="La mas alta calidad en botas para trabajo pesado"
+                imageSrc="/heroImg.webp"
+                imageAlt="Sample image"
+            >
+                <Button size="xl" asChild>
+                    <a href="#">Comprar</a>
+                </Button>
+            </LandingPrimaryImageCtaSection>;
         </section>
     )
 }
--- src/app/components/navbar/navbar.tsx
+++ src/app/components/navbar/navbar.tsx
@@ -1,5 +1,4 @@
 import { Button } from "@chakra-ui/react"
-
 import { routes } from "./routes"
 
 export default function Navbar() {
