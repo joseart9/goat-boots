9ef48f4eb5bbf86924a09743c36756153cc58199
Arturo <a01177145@tec.mx>
2025-05-15 19:17:48 -0600

    increase body size limit

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   experimental: {
     optimizePackageImports: ["@chakra-ui/react"],
+    serverActions: {
+      bodySizeLimit: "20mb",
+    },
   },
   images: {
     remotePatterns: [
